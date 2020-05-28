62e06685675afca6e98432a8e865e9c07fbd9f14
Nick-LGD <n.legrand77190@gmail.com>
2020-05-28 09:27:00 +0200

    quete 10

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -1,14 +1,11 @@
 <?php
 
 namespace App\Entity;
-
-use App\Repository\ProgramRepository;
+use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-
 /**
- * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\ProgramRepository")
  */
 class Program
 {
@@ -28,61 +25,17 @@ class Program
      * @ORM\Column(type="text")
      */
     private $summary;
-
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Season", mappedBy="program")
+     */
+    private $seasons;
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $poster;
 
-    private $season;
-
-    public function __construct()
-    {
-        $this->season = new ArrayCollection();
-    }
-
     /**
-     * @return Collection|Season[]
-     */
-    public function getSeasons(): Collection
-    {
-        return $this->seasons;
-    }
-
-    /**
-     * param Season $season
-     * @return Program
-     */
-    public function addSeason(Season $season): self
-    {
-        if (!$this->seasons->contains($season)) {
-            $this->seasons[] = $season;
-            $season->setProgram($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param Season $season
-     * @return Program
-     */
-
-    public function removeSeason(Season $season): self
-    {
-        if ($this->seasons->contains($season)) {
-            $this->seasons->removeElement($season);
-            // set the owning side to null (unless already changed)
-            if ($season->getProgram() === $this) {
-                $season->setProgram(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="programs")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="programs")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
---
+++ src/Migrations/Version20200526132440.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200526132440 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE program DROP country, DROP year');
+        $this->addSql('ALTER TABLE season CHANGE id id INT AUTO_INCREMENT NOT NULL, ADD PRIMARY KEY (id)');
+        $this->addSql('ALTER TABLE season ADD CONSTRAINT FK_F0E45BA93EB8070A FOREIGN KEY (program_id) REFERENCES program (id)');
+        $this->addSql('CREATE INDEX IDX_F0E45BA93EB8070A ON season (program_id)');
+        $this->addSql('ALTER TABLE episode CHANGE id id INT AUTO_INCREMENT NOT NULL, ADD PRIMARY KEY (id)');
+        $this->addSql('ALTER TABLE episode ADD CONSTRAINT FK_DDAA1CDA4EC001D1 FOREIGN KEY (season_id) REFERENCES season (id)');
+        $this->addSql('CREATE INDEX IDX_DDAA1CDA4EC001D1 ON episode (season_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE episode MODIFY id INT NOT NULL');
+        $this->addSql('ALTER TABLE episode DROP FOREIGN KEY FK_DDAA1CDA4EC001D1');
+        $this->addSql('DROP INDEX IDX_DDAA1CDA4EC001D1 ON episode');
+        $this->addSql('ALTER TABLE episode DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE episode CHANGE id id INT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD country VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD year INT NOT NULL');
+        $this->addSql('ALTER TABLE season MODIFY id INT NOT NULL');
+        $this->addSql('ALTER TABLE season DROP FOREIGN KEY FK_F0E45BA93EB8070A');
+        $this->addSql('DROP INDEX IDX_F0E45BA93EB8070A ON season');
+        $this->addSql('ALTER TABLE season DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE season CHANGE id id INT NOT NULL');
+    }
+}
